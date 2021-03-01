2f6903f023ce0642a78ac6aeb154a65f26a950d3
imjack <imjack@users.noreply.github.com>
2021-03-01 00:05:16 +0000

    clean up rename cmd

--- src/main/java/cn/yescallop/essentialsnk/command/defaults/RepairCommand.java
+++ src/main/java/cn/yescallop/essentialsnk/command/defaults/RepairCommand.java
@@ -8,40 +8,40 @@ import cn.yescallop.essentialsnk.EssentialsAPI;
 import cn.yescallop.essentialsnk.Language;
 import cn.yescallop.essentialsnk.command.CommandBase;
 
-
 public class RepairCommand extends CommandBase {
-	
-	private final Item token;
-    public RepairCommand(EssentialsAPI api) {
-        super("repair", api);
-        token = new Item(Item.FIREWORKSCHARGE, 0, 1, TextFormat.GREEN + "Repair Token");
-        token.setLore("Do /repair with the tool in your hand to fix it.");
-        this.setAliases(new String[]{"fix"});
-        // command parameters
-        commandParameters.clear();
-    }
 
-    public boolean execute(CommandSender sender, String label, String[] args) {
-        if (!this.testPermission(sender)) {
-            return false;
-        }
-        if (!this.testIngame(sender)) {
-            return false;
-        }
-        Player player = (Player) sender;
-        if(!player.getInventory().contains(token)) {
-            player.sendMessage(Language.translate("commands.repair.length"));
-        	return true;
-        }
-        Item handItem = player.getInventory().getItemInHand();
-        if (!api.isRepairable(handItem)) {
-            player.sendMessage(Language.translate("commands.repair.unrepairable"));
-        	return true;
-        }
-        player.getInventory().remove(token);
-        handItem.setDamage(0);
-        player.getInventory().setItemInHand(handItem);
-        player.sendMessage(Language.translate("commands.repair.success"));
-        return true;
-    }
+	private final Item token;
+
+	public RepairCommand(EssentialsAPI api) {
+		super("repair", api);
+		token = new Item(Item.FIREWORKSCHARGE, 0, 1, TextFormat.GREEN + "Repair Token");
+		token.setLore("Do /repair with the tool in your hand to fix it.");
+		this.setAliases(new String[] { "fix" });
+		// command parameters
+		commandParameters.clear();
+	}
+
+	public boolean execute(CommandSender sender, String label, String[] args) {
+		if (!this.testPermission(sender)) {
+			return false;
+		}
+		if (!this.testIngame(sender)) {
+			return false;
+		}
+		Player player = (Player) sender;
+		if (!player.getInventory().contains(token)) {
+			player.sendMessage(Language.translate("commands.repair.length"));
+			return true;
+		}
+		Item handItem = player.getInventory().getItemInHand();
+		if (!api.isRepairable(handItem)) {
+			player.sendMessage(Language.translate("commands.repair.unrepairable"));
+			return true;
+		}
+		player.getInventory().remove(token);
+		handItem.setDamage(0);
+		player.getInventory().setItemInHand(handItem);
+		player.sendMessage(Language.translate("commands.repair.success"));
+		return true;
+	}
 }
